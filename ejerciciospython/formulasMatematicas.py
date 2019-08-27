33a29440b875c91e4ef686835626500da6f93811
Marcos Monjon Mas <sysmarc13@hotmail.com>
2019-08-27 20:03:40 +0200

    subimos archivo de formulas matemáticas básicas

---
+++ ejerciciospython/formulasMatematicas.py
@@ -0,0 +1,35 @@
+
+print (1+1)
+
+print (10-5)
+
+print (5*5)
+
+print (10/2)
+
+
+print (2**3)
+
+
+print ( 4%2)
+
+
+
+
+def sumar(num1,num2):
+
+	return num1 + num2
+
+print (sumar(5,20))
+
+
+
+def restar(num1,num2):
+
+	return num1 - num2
+
+
+
+print (restar(50,5))
+
+
