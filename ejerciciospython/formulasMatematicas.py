651a5f2648dda6f0d02a96f2ffd249b85020e3b6
Marcos Monjon Mas <sysmarc13@hotmail.com>
2019-08-28 20:26:05 +0200

    Subimos ejercicios operadores matemáticos

--- ejerciciospython/formulasMatematicas.py
+++ ejerciciospython/formulasMatematicas.py
@@ -1,4 +1,32 @@
-print (1+1)
+
+print (2+2)
+
+print (5-2)
+
+print (2*2)
+
+print (10/2)
+
+print (2**3)
+
+print (24%2)
 
 
-print (2+2)asasasasasas
+def sumar(numero1,numero2):
+
+
+	return numero1 + numero2
+
+
+
+print(sumar(10,20)) 
+
+
+def multiplicar(numeroUno,numeroDos):
+
+
+	return numeroUno * numeroDos
+
+
+
+print(multiplicar(5,6))
