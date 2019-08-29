c51a240a6ab69db85e4072ca0caff566d281d1a6
Marcos Monjon Mas <sysmarc13@hotmail.com>
2019-08-29 18:16:06 +0200

    subimos ejercicios de primeras funciones

---
+++ ejerciciosPython/primerasFunciones.py
@@ -0,0 +1,21 @@
+
+
+def multiplicar(numeroUno,numeroDos):
+
+	return numeroUno * numeroDos
+
+
+
+print (multiplicar(9,3))
+
+
+def sumar(numeroUno,numeroDos):
+
+
+	return numeroUno + numeroDos
+
+
+
+
+print (sumar(2,2))
+
